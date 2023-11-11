1794e61c7ca8f6500b8c592cde814223699b4fe5
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2023-11-11 17:21:06 +0100

    Challenge 02

---
+++ src/challenge02/challenge02.js
@@ -0,0 +1,20 @@
+export function challenge02(message) {
+  if (typeof message !== 'string') {
+    throw new Error('challenge02() expects a string')
+  }
+  let result = ''
+  let carry = 0
+  for (let i = 0; i < message.length; i++) {
+    const char = message[i]
+    if (char === '#') {
+      carry++;
+    } else if (char === '@') {
+      carry--;
+    } else if (char === '*') {
+      carry = carry * carry;
+    } else if (char === '&') {
+      result += carry + ''
+    } 
+  }
+  return result
+}
---
+++ tests/challenge02.test.js
@@ -0,0 +1,33 @@
+import { describe, it, expect } from 'vitest'
+import { challenge02 } from '../src/challenge02/challenge02'
+
+
+describe('challenge02', () => {
+  it('should recieve a string', () => {
+    expect(() => challenge02()).toThrow()
+    expect(() => challenge02(1)).toThrow()
+    expect(() => challenge02([])).toThrow()
+    expect(() => challenge02({})).toThrow()
+    expect(() => challenge02('')).not.toThrow()
+  })
+
+  it('should return a string', () => {
+    expect(challenge02('')).to.be.a('string')
+  })
+
+  it('should return "0" with "&"', () => {
+    expect(challenge02('&')).toBe('0');
+  })
+
+  it('should return "00" with "&&', () => {
+    expect(challenge02('&')).toBe('0');
+  })
+
+  it('should return "4" with "##*&"', () => {
+    expect(challenge02('##*&')).toBe('4');
+  });
+
+  it('should return "0243" with "&##&*&@&"', () => {
+    expect(challenge02('&##&*&@&')).toBe('0243');
+  });
+})
